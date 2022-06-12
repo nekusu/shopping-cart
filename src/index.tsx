365175ac6eaa20350e9c0d68957f06942746e256
nekusu <nekusu.dev@gmail.com>
2022-06-12 05:01:19 -0300

    Add BrowserRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './fonts/GTWalsheimPro/stylesheet.css';
 import './scss/index.scss';
@@ -7,6 +8,10 @@ import './scss/index.scss';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 );
