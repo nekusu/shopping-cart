415e51e8860f3432d2c2e51b65757634c1ac099d
nekusu <nekusu.dev@gmail.com>
2022-06-12 04:58:52 -0300

    Reset CSS default styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './fonts/GTWalsheimPro/stylesheet.css';
+import './scss/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
