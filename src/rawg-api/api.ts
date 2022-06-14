7e1100a495292d3072c50e111745f97ae458c797
nekusu <nekusu.dev@gmail.com>
2022-06-14 01:52:21 -0300

    Implement API request caching

--- src/rawg-api/api.ts
+++ src/rawg-api/api.ts
@@ -3,6 +3,9 @@ const api = {
   key: '2aa8dadc5e564d299ec1818bf9a9c379',
 };
 
+const cachedRequests: Record<string, unknown> =
+  JSON.parse(localStorage.getItem('cachedRequests') || '{}');
+
 interface ResponseSchema<T> {
   count: number,
   next: string,
@@ -11,10 +14,13 @@ interface ResponseSchema<T> {
 }
 
 async function get<T>(endpoint: string, params?: Record<string, string>): Promise<T> {
-  const searchParams = new URLSearchParams(params);
+  const searchParams = new URLSearchParams(params).toString();
+  if (cachedRequests[searchParams]) return cachedRequests[searchParams] as Promise<T>;
   const response = await fetch(`${api.url}${endpoint}?${searchParams}&key=${api.key}`);
   if (!response.ok) throw new Error(response.statusText);
   const data = await response.json();
+  cachedRequests[searchParams] = data;
+  localStorage.setItem('cachedRequests', JSON.stringify(cachedRequests));
   return data;
 }
 
