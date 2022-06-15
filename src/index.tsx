c150012f8840aae0826fd115d97a4f2db33f01f4
nekusu <nekusu.dev@gmail.com>
2022-06-15 03:23:41 -0300

    Replace BrowserRouter with HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './fonts/GTWalsheimPro/stylesheet.css';
 import './scss/index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/*" element={<App />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
