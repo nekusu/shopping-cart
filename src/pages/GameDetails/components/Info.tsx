f1ed8ea170ccb1a6fe59357ee5602744b2fbd28e
nekusu <nekusu.dev@gmail.com>
2022-06-15 02:32:06 -0300

    Improve responsive design

--- src/pages/GameDetails/components/Info.tsx
+++ src/pages/GameDetails/components/Info.tsx
@@ -1,4 +1,8 @@
-import { Transition } from '../../../components';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import { useScrollTo } from 'framer-motion-scroll-to-hook';
+import { RiArrowDownSLine, RiArrowUpSLine } from 'react-icons/ri';
+import { Transition, Button } from '../../../components';
 import { Game } from '../../../types/Game.types';
 
 function Info({ game }: { game: Game }) {
@@ -9,22 +13,58 @@ function Info({ game }: { game: Game }) {
     platforms,
     genres,
     website,
+    developers,
+    publishers,
   } = game;
+  const [isExpanded, setIsExpanded] = useState(false);
+  const scrollTo = useScrollTo();
 
   return (
     <Transition className="Info">
       <div className="About">
         <h4>About</h4>
-        <p>{description_raw}</p>
+        {description_raw.split('###').map((p, index) => (
+          <p key={index}>{p}</p>
+        ))}
       </div>
-      <div className="MoreInfo">
-        <a href={website} target="_blank" rel="noreferrer">
-          {name} Website
-        </a>
-        <p>Released: {new Date(released).toLocaleDateString()}</p>
-        <p>Platforms: {platforms.map((p) => p.platform.name).join(', ')}</p>
-        <p>Genres: {genres.map((g) => g.name).join(', ')}</p>
-      </div>
+      <motion.div
+        className="MoreInfo"
+        initial={false}
+        animate={{ minHeight: isExpanded ? '220px' : '60px' }}
+      >
+        {isExpanded
+          ? <>
+            <Transition
+              key="expanded"
+              className="Expanded"
+              direction="up"
+              distance={30}
+            >
+              <a href={website} target="_blank" rel="noreferrer">
+                {name} Website
+              </a>
+              <p>Released: {new Date(released).toLocaleDateString()}</p>
+              <p>Platforms: {platforms.map((p) => p.platform.name).join(', ')}</p>
+              <p>Genres: {genres.map((g) => g.name).join(', ')}</p>
+              <p>Developers: {developers.map((d) => d.name).join(', ')}</p>
+              <p>Publishers: {publishers.map((p) => p.name).join(', ')}</p>
+            </Transition>
+            <motion.div className="Expand" layoutId="expand-button">
+              <Button handleClick={() => setIsExpanded(false)}>
+                Hide <RiArrowUpSLine />
+              </Button>
+            </motion.div>
+          </>
+          : <motion.div className="Expand" layoutId="expand-button">
+            <Button handleClick={() => {
+              setIsExpanded(true);
+              scrollTo(10000);
+            }}>
+              More <RiArrowDownSLine />
+            </Button>
+          </motion.div>
+        }
+      </motion.div>
     </Transition>
   );
 }
