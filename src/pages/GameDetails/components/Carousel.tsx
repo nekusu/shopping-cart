218a7b0ffc96e25cc91dc13fec0c0892060b8c92
nekusu <nekusu.dev@gmail.com>
2022-06-24 18:26:51 -0300

    Add drag gesture to Carousel

--- src/pages/GameDetails/components/Carousel.tsx
+++ src/pages/GameDetails/components/Carousel.tsx
@@ -1,5 +1,5 @@
 import { Children, useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, PanInfo, useAnimation } from 'framer-motion';
 import { Button } from '../../../components';
 import {
   RiArrowLeftSLine,
@@ -17,30 +17,55 @@ function Carousel(props: Props) {
   const { children, duration, useDots = true } = props;
   const [index, setIndex] = useState(0);
   const childrenArray = Children.toArray(children);
+  const carousel = useRef<HTMLDivElement>(null);
+  const carouselControls = useAnimation();
+  const animateCarousel = () => {
+    carouselControls.start({ x: `${-index * 100}%` });
+  };
   const timeout = useRef<NodeJS.Timer>();
+  const setIndexByTimeout = () => {
+    timeout.current = setTimeout(() => {
+      setIndex((index) => index + 1);
+    }, duration * 1000);
+  };
+  const setIndexByPosition = (_event: never, info: PanInfo) => {
+    const { x } = info.offset;
+    const carouselWidth = carousel.current?.clientWidth || 1;
+    const initialOffset = index * carouselWidth;
+    const newIndex = Math.round((initialOffset + x * -2) / carouselWidth);
+    if (newIndex !== index) {
+      setIndex(newIndex);
+    } else {
+      animateCarousel();
+      setIndexByTimeout();
+    }
+  };
 
   useEffect(() => {
+    animateCarousel();
     if (index >= childrenArray.length) {
       setIndex(0);
     } else if (index < 0) {
       setIndex(childrenArray.length - 1);
     }
-    timeout.current = setTimeout(() => {
-      setIndex((index) => index + 1);
-    }, duration * 1000);
+    setIndexByTimeout();
     return () => clearTimeout(timeout.current);
   }, [index]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
-    <div className="Carousel">
+    <div ref={carousel} className="Carousel">
       <Button handleClick={() => setIndex((index) => index - 1)}>
         <RiArrowLeftSLine />
       </Button>
       <motion.div
         className="Items"
+        drag="x"
+        dragMomentum={false}
+        onDragStart={() => clearTimeout(timeout.current)}
+        onDragEnd={setIndexByPosition}
         initial={{ x: 0 }}
-        animate={{ x: `${-index * 100}%` }}
-        transition={{ duration: 0.75 }}
+        animate={carouselControls}
+        transition={{ duration: 0.6 }}
       >
         {children}
       </motion.div>
@@ -49,19 +74,18 @@ function Carousel(props: Props) {
           {childrenArray.map((_, i) => (
             <motion.div
               key={`dot-${i}`}
-              className={'Dot'}
+              className="Dot"
               initial={false}
               animate={{ scale: +(index !== i) }}
-              transition={{ type: 'spring', duration: 0.01 }}
+              transition={{ duration: 0.2 }}
               onClick={() => setIndex(i)}
             />
           ))}
           <motion.div
-            layout
-            className={'Dot Active'}
+            className="Dot active"
             initial={false}
             animate={{ x: index * 22 }}
-            transition={{ type: 'spring', duration: 0.01 }}
+            transition={{ duration: 0.35 }}
           />
         </div>
       )}
