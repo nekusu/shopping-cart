0426ce11a9064180d4fb888c19d8d917db9643eb
nekusu <nekusu.dev@gmail.com>
2022-06-28 15:05:20 -0300

    Minor improvements

--- src/utils/getPrice.ts
+++ src/utils/getPrice.ts
@@ -3,15 +3,19 @@ import { Game } from '../types/Game.types';
 function getPrice(game: Game) {
   const { released } = game;
   const isIndie = !!game.genres.find((g) => g.name === 'Indie');
-  const minPrice = 2;
   const releaseYear = new Date(released).getFullYear();
   const currentYear = new Date().getFullYear();
   const yearsDifference = currentYear - releaseYear;
-  let discountPerYear = isIndie ? 0.3 : 0.375;
+  const minPrice = 1;
+  let discountPerYear = 0.35;
   let newPrice = isIndie ? 30 : 70;
   for (let i = 0; i < yearsDifference; i++) {
     newPrice *= 1 - discountPerYear;
-    discountPerYear -= 0.02;
+    if (discountPerYear > 0.1) {
+      discountPerYear -= 0.08;
+    } else {
+      discountPerYear = 0.1;
+    }
   }
   newPrice = Math.ceil(newPrice);
   newPrice = newPrice < minPrice ? minPrice : newPrice;
